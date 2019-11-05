2e01af4cdcc6ee5984578a152f27583fdd958ddc
Krishna Ananthi <tkrishnaananthi@gmail.com>
2019-11-05 15:45:22 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {SharedModule} from '@shared/shared.module';
+import {CoreModule} from '@core/core.module';
 import {ConfiguratorModule} from '@configurator/configurator.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, ConfiguratorModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule, ConfiguratorModule],
   providers: [],
   bootstrap: [AppComponent],
 })
